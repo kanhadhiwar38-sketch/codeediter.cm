2832183ccccfbfd5b80e0a99da9c405a4deb4248
kanhadhiwar38-sketch <kanhadhiwar38@gmail.com>
2026-08-26 16:30:03 +0530

    kanha

---
+++ android/app/src/main/java/com/codeeditor/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.codeeditor.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBackground = Color(0xFF1E1E1E)
+val DarkSurface = Color(0xFF252526)
+val PrimaryBlue = Color(0xFF007ACC)
+val AccentPurple = Color(0xFFA855F7)
+val TextWhite = Color(0xFFD4D4D4)
+val BorderDark = Color(0xFF2B2B2B)
